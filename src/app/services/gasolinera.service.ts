0cbaf69155dd82cd63efc412219896ad44e71f8a
ppsegur <pepesegurarodriguez@gmail.com>
2024-11-06 18:32:20 +0100

    Servicio de filtrado

--- src/app/services/gasolinera.service.ts
+++ src/app/services/gasolinera.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gasolinera } from '../interfaces/gasolinera.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,29 @@ export class GasolineraService {
     return this.http.get(
       'https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/'
     );
+  }
+  filterGasList(
+    gasolineras: Gasolinera[],
+    tipoCombustible: string,
+    precioMin: number,
+    precioMax: number,
+    postalCode: string,
+    provincias: string[]
+  ): Gasolinera[] {
+    return gasolineras.filter((gasolinera) => {
+      const precio =
+        tipoCombustible === 'gasolina'
+          ? gasolinera.price95
+          : gasolinera.priceDiesel;
+      const matchesPostalCode = postalCode ? gasolinera.postalCode === postalCode : true;
+      const matchesProvincia = provincias.length > 0 ? provincias.some(provincia => gasolinera.postalCode.startsWith(provincia)) : true;
+      return (
+        precio >= precioMin &&
+        precio <= precioMax &&
+        matchesPostalCode &&
+        matchesProvincia
+      );
+    });
   }
 
 }
